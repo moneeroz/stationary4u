1fad76904558419f447294dda5998839377a2541
Moneer <moneer.alromimah@gmail.com>
2023-03-21 01:19:02 -0600

    added more products

--- src/config/products.ts
+++ src/config/products.ts
@@ -9,21 +9,69 @@ const products: Iproduct[] = [
   },
   {
     id: 2,
-    name: 'Smart Hard Cover Notebook',
+    name: 'Aluminum Binder',
+    img: 'redbinder.webp',
+    price: 75,
+  },
+  {
+    id: 3,
+    name: 'Excutive Binder',
+    img: 'leatherbinder.jpg',
+    price: 100,
+  },
+  {
+    id: 4,
+    name: '1-Subject Notebook',
+    img: 'subjectnotebook.webp',
+    price: 2.25,
+  },
+  {
+    id: 5,
+    name: 'Spiral Notebook',
+    img: 'spiralnotebook.webp',
+    price: 75,
+  },
+  {
+    id: 6,
+    name: 'Smart Notebook',
     img: 'notebook.jpg',
     price: 326,
   },
   {
-    id: 3,
+    id: 7,
     name: 'Felt-Tip Pens',
     img: 'pen.webp',
     price: 15,
   },
   {
-    id: 4,
+    id: 8,
+    name: 'Retractable Pens',
+    img: 'gelpen.webp',
+    price: 31,
+  },
+  {
+    id: 9,
+    name: 'Rollerball Pen',
+    img: 'rollerpen.webp',
+    price: 118,
+  },
+  {
+    id: 10,
+    name: 'Desktop Stapler',
+    img: 'deskstapler.webp',
+    price: 22,
+  },
+  {
+    id: 11,
     name: 'Half Strip Stapler',
     img: 'stapler.webp',
     price: 93,
+  },
+  {
+    id: 12,
+    name: 'Electric Stapler',
+    img: 'elecrticstapler.webp',
+    price: 281,
   },
 ];
 
